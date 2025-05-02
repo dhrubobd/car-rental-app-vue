9698fb7afa5a0cff282bf866ef657839bdb3481b
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-05-03 00:49:27 +0600

    Seeder is Made For Users and Cars

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,91 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            [
+                'name' => 'Harry Potter',
+                'email' => 'admin@admin.com',
+                'password' => 'admin',
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Hasnat Abdullah',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Dipty Chowdhruy',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Sarjis Alom',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+            [
+                'name' => 'Sadia Sraboni',
+                'email' => Str::random(10).'@example.com',
+                'password' => Str::random(7),
+                'role' => 'customer',
+            ],
+        ]);
+
+        DB::table('cars')->insert([
+            [
+                'name' => 'Axio',
+                'brand' => 'Toyota',
+                'model' => '2015',
+                'year' => random_int(2010,2023),
+                'car_type' => 'Sedan',
+                'daily_rent_price' => '100',
+                'availability' => true,
+                'image' => 'uploads/car1.jpg',
+            ],
+            [
+                'name' => 'BMW iX',
+                'brand' => 'BMW',
+                'model' => 'MY25-iX',
+                'year' => random_int(2020,2023),
+                'car_type' => 'SUV Electric',
+                'daily_rent_price' => '200',
+                'availability' => true,
+                'image' => 'uploads/car2.jpg',
+            ],
+            [
+                'name' => 'CH-R',
+                'brand' => 'Toyota',
+                'model' => '2018',
+                'year' => random_int(2018,2023),
+                'car_type' => 'SUV',
+                'daily_rent_price' => '150',
+                'availability' => true,
+                'image' => 'uploads/car3.jpg',
+            ],
+            [
+                
+                'name' => 'Almera',
+                'brand' => 'Nissan',
+                'model' => '2022',
+                'year' => random_int(2022,2024),
+                'car_type' => 'Sedan',
+                'daily_rent_price' => '120',
+                'availability' => true,
+                'image' => 'uploads/car4.jpg',
+            ],
+            [
+                'name' => 'Model Y',
+                'brand' => 'Tesla',
+                'model' => '2023',
+                'year' => random_int(2023,2024),
+                'car_type' => 'Sedan Electric',
+                'daily_rent_price' => '180',
+                'availability' => true,
+                'image' => 'uploads/car5.jpg',
+            ],
         ]);
     }
 }
